042ce21342346cb4c0aa18a096fa45c21d7bfb63
unknown <martinaoyhenard@gmail.com>
2019-05-25 00:05:27 +0200

    week folder added

---
+++ Week2/java1.js
@@ -0,0 +1,9 @@
+console.log = ("Hola Mundo"); // Spanish
+console.log = ("Hello World"); // English
+console.log = ("Hej världen"); // Swedish
+
+console.log('I\'m awesome');
+
+let x;
+console.log("value of my variable x will be: undefined");
+console.log(x);
