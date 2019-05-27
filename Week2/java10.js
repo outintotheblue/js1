9920d6372b1597cbf77eb7aebc0b15e14dcb63b5
unknown <martinaoyhenard@gmail.com>
2019-05-27 13:19:06 +0200

    js exercises done

---
+++ Week2/java10.js
@@ -0,0 +1,10 @@
+let myArray = ['Yes, an array can store numbers like: ', 44, 55, 66, 'or a string like "I am a string" '];
+console.log(myArray);
+
+console.log("Compare Infinities");
+
+let a = 6 / 0;
+let b = 10 / 0;
+if (a === b) { console.log("Its the same"); } else { console.log("it is not the same"); }
+
+console.log("I really dont know if understood this :D ");
