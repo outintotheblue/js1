6968474678e9f9c8749404865457d8cc16382e7f
unknown <martinaoyhenard@gmail.com>
2019-06-01 10:31:41 +0200

    .

--- Week 3/1strings.js
+++ Week 3/1strings.js
@@ -2,10 +2,7 @@ let myString = "hello,this,is,a,difficult,to,read,sentence";
 
 console.log(myString);
 
-myString.length;
+console.log(myString.length);
 
 let newString = myString.replace(/,/g, " ");
 console.log(newString);
-let x = 5;
-
-console.log(x);
